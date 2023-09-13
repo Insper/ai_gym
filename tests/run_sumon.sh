a53b791693b4f7056f4578209ad8a5e5006f3ee8
Fabrício J. Barth <fabricio.barth@gmail.com>
2023-09-13 15:12:44 -0300

    criando estrutura inicial para testar os metodos de poda (#17)

--- tests/run_sumon.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# you must change this path
-export PYTHONPATH=/home/fbarth/workspaces/ai_gym/
-
-python SumOne.py
---
+++ tests/test_poda_BP.py
@@ -0,0 +1,18 @@
+#
+# Considerando as implementações: 
+#
+# - AspiradorPo.py
+# - SumOne.py
+# - poi.py
+#
+# Testar o algoritmo: 
+#
+# 2) Depth-first search (BuscaProfundidade)
+#
+# Com as seguintes configurações de poda: 
+#
+# - without pruning
+# - father-son pruning
+# - general pruning
+#
+#
