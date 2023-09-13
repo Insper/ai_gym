24be678210d84f4498c87130cfb5d3312eafc844
Fabrício J. Barth <fabricio.barth@gmail.com>
2023-09-13 15:12:44 -0300

    criando estrutura inicial para testar os metodos de poda (#17)

---
+++ tests/test_poda_BCU.py
@@ -0,0 +1,18 @@
+#
+# Considerando as implementações: 
+#
+# - AspiradorPo.py
+# - SumOne.py
+# - poi.py
+#
+# Testar o algoritmo: 
+#
+# 4) Uniform cost search (CustoUniforme)
+#
+# Com as seguintes configurações de poda: 
+#
+# - without pruning
+# - father-son pruning
+# - general pruning
+#
+#
