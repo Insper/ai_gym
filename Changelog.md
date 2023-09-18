d7a6a897dbe5a0c686b4f0b1d59a336e0cd9b0a6
Fabricio J Barth <fabricio.barth@gmail.com>
2023-09-18 11:08:34 -0300

    version 0.1.5

---
+++ Changelog.md
@@ -0,0 +1,13 @@
+# Change log
+
+All notable changes to this project will be documented in this file.
+
+## [0.1.5] 2023-09-18
+
+### Added
+
+* Pruning option in search method for all algorithms in SearchAlgorithms.py. 
+
+### Changed
+
+### Fixed
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='aigyminsper',
-    version='0.1.4',
+    version='0.1.5',
     packages=['aigyminsper','aigyminsper.search'],
     install_requires=[],
     description='This package implements basic AI algorithms',
