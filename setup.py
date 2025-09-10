f1cf88f1161baa303f91911c1cccfe2a65d0314e
marcelopenas <marcelopenas@outlook.com>
2025-09-10 08:39:52 -0300

    add requirements to package

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ setup(
     name="aigyminsper",
     version="0.2.4",
     packages=['aigyminsper','aigyminsper.search'],
+    install_requires=[
+        "graphviz ==0.21",
+        "matplotlib ==3.10.6",
+        "networkx ==3.5",
+        "pydot ==4.0.1",
+        "pyparsing ==3.2.3",
+    ],
     author='Fabricio Barth',
     author_email='fabriciojb@insper.edu.br',
     description='A libray that helps you to learn Artificial Intelligence.',
