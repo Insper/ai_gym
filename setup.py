77c38c726a1268bb2a27b21e959979c09c988ecf
Fabrício J. Barth <fabricio.barth@gmail.com>
2025-09-12 18:28:03 -0300

    adding details about the installing on windows and mac, and also updating the version of the package (#69)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aigyminsper",
-    version="0.2.4",
+    version="0.2.5",
     packages=['aigyminsper','aigyminsper.search'],
     install_requires=[
         "graphviz ==0.21",
